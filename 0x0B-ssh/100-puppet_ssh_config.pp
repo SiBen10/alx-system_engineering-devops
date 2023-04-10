333ea14c5271a9b53846d4e74aa48d4b11eec578
SiBen10 <benjaminkn805@gmail.com>
2023-04-10 15:07:32 -0700

    updating web server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -11,6 +11,6 @@ file_line { 'Turn off passwd auth':
 file_line { 'Delare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
+  line   => '     IdentityFile ~/.ssh/id_rsa',
   replace => true,
 }
