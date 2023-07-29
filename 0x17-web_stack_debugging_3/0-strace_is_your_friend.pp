948cbe7a454c1173657d58dd2aabf1e60f3e5c26
Abodunrin Benjamin Akin <87836638+SiBen10@users.noreply.github.com>
2023-07-29 13:43:37 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
-
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-}
